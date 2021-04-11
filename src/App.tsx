dc3b0acebe15f01051102eed6c89452562be8a64
Brandon Manzo <brandong.manzog@gmail.com>
2021-04-11 01:57:06 -0600

    Delete unnecesary objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FunctionComponent } from "react";
+import { ThemeProvider } from "styled-components";
+import { AboutMe } from "./components/aboutMe";
+import { Navbar } from "./components/navbar";
+import GlobalStyles from "./styles/globalStyle";
+import myTheme from "./styles/theme";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FunctionComponent = () => (
+  <ThemeProvider theme={myTheme}>
+    <GlobalStyles />
+    <Navbar />
+    <AboutMe />
+  </ThemeProvider>
+);
 
 export default App;
