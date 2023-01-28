07ed517bf8400424369a64838abd8febce67e8f5
box-bm <brandong.manzog@icloud.com>
2023-01-28 10:53:17 -0600

    create base router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles, darkTheme, lightTheme } from "./themes";
 import useThemeDetector from "./hooks/useThemeDetector";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const darkMode = useThemeDetector();
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (location.pathname === "/") {
-      navigate("home");
-    }
-  }, [location, navigate]);
 
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
