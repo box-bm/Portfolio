5ac67a34bd740d1dd13b99f60f92fbc4d9f30665
Brandon Manzo <brandong.manzog@icloud.com>
2023-03-27 10:24:07 -0600

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import lightTheme from "@/theme/lightTheme";
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -17,6 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
     >
       <NextUIProvider>
         <Component {...pageProps} />
+        <Analytics />
       </NextUIProvider>
     </NextThemesProvider>
   );
