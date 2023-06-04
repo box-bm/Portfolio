01ce9c121086585cee33ca3c44d5fac4d633acb6
Brandon Manzo <brandong.manzog@icloud.com>
2023-06-03 22:46:46 -0600

    added assets to my website

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,9 @@
-import { Divider, Navbar as NextNavbar, Text } from "@nextui-org/react";
+import {
+  Divider,
+  Navbar as NextNavbar,
+  Text,
+  useTheme,
+} from "@nextui-org/react";
 import { useRef } from "react";
 import socialMedia from "../../lib/data/socialMedia";
 import { useRouter } from "next/router";
@@ -6,12 +11,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Box } from "./box";
 import Link from "next/link";
 import SitesProps from "../../lib/models/sitesProps";
+import Image from "next/image";
 
 type Props = SitesProps;
 
 const Navbar = ({ externalSites, sites }: Props) => {
   const { asPath } = useRouter();
   const navbarToggleRef = useRef<HTMLButtonElement>(null);
+  const theme = useTheme();
 
   return (
     <NextNavbar
@@ -22,9 +29,15 @@ const Navbar = ({ externalSites, sites }: Props) => {
     >
       <NextNavbar.Brand>
         <NextNavbar.Toggle showIn="xs" ref={navbarToggleRef} />
-        <Link href="/">
-          <Text b h4 css={{ margin: 0, "@smMax": { marginLeft: 12 } }}>
-            BoxM
+        <Link href="/" style={{ display: "flex" }}>
+          <Image
+            src={theme.isDark ? "/logo_white.png" : "/logo_colour.png"}
+            alt="Box-Dev logo"
+            width={60}
+            height={60}
+          />
+          <Text b h4 css={{ margin: 12 }}>
+            Box Dev
           </Text>
         </Link>
       </NextNavbar.Brand>
